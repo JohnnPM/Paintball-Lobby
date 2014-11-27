e652a1415ac42c213681ee7c3d7607b1710b8f08
xXJarJarXx <598Johnn897@gmail.com>
2014-11-27 18:01:33 -0500

    Update 11/27/14

---
+++ src/paintball/lobby/PaintballLobby.java
@@ -0,0 +1,31 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball.lobby
+ *
+ */
+package paintball.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ */
+public class PaintballLobby extends JavaPlugin
+{
+	@Override public void onLoad()
+	{
+		
+	}
+	
+	@Override public void onEnable()
+	{
+		
+	}
+	
+	@Override public void onDisable()
+	{
+		
+	}
+}
---
+++ src/paintball/lobby/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Nov 27, 2014
+ * Package: paintball.lobby
+ *
+ */
+/**
+ * 
+ */
+package paintball.lobby;
